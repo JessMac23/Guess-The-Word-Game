b3973a20e045f5ba8fea6fe4463554d6481c9959
Jessica M <jezkaa@gmail.com>
2023-11-05 15:16:23 -0600

    First Commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuessesElement = document.querySelector(".remaining");
+const reminaingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const placeholder = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessLetterButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+});
+
+
